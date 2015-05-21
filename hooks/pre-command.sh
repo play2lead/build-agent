5f34374b5d74311446563095c4d9c257d346b1e1
Maxim Filimonov <tpaktopsp@gmail.com>
2015-05-21 11:33:24 +1000

    show error when template file is not specified

--- hooks/pre-command.sh
+++ hooks/pre-command.sh
@@ -7,6 +7,13 @@ set -e
 if [[ ! -z "${BUILDKITE_DOCKER_COMPOSE_CONTAINER:-}" ]] && [[ "$BUILDKITE_DOCKER_COMPOSE_CONTAINER" != "" ]]; then
   # If user specified docker compose file we need to generate
   if [[ ! -f $BUILDKITE_DOCKER_COMPOSE_FILE ]]; then
-    echo -e "$(eval "echo -e \"`<$BUILDKITE_DOCKER_COMPOSE_FILE.template`\"")" >> $BUILDKITE_DOCKER_COMPOSE_FILE
+    TEMPLATE_FILE="$BUILDKITE_DOCKER_COMPOSE_FILE.template"
+    if [[ -f $TEMPLATE_FILE ]]; then
+      echo -e "$(eval "echo -e \"`<$TEMPLATE_FILE`\"")" >> $BUILDKITE_DOCKER_COMPOSE_FILE
+      exit 0
+    else
+      echo "template file $TEMPLATE_FILE not found"
+      exit 1
+    fi
   fi
 fi
